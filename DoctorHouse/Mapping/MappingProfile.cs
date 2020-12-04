3d7c7d27fa8eaed7330563c8651f17afd40fb066
kettu0 <aneta.kruc@gmail.com>
2020-12-05 00:11:03 +0100

    create map for users

---
+++ DoctorHouse/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DoctorHouse.Controllers.Resources;
+using DoctorHouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorHouse.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResource>();
+            CreateMap<UserDetails, UserDetails>();
+            CreateMap<Specialist, SpecialistResource>();
+            CreateMap<Customer, CustomerResource>();
+        }
+    }
+}
