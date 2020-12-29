78cf61d0c3db582760267d83d9b826f1ff88f0f3
kettu0 <aneta.kruc@gmail.com>
2020-12-29 23:04:55 +0100

    correct UserResource

--- DoctorHouse/Models/User.cs
+++ DoctorHouse/Models/User.cs
@@ -5,20 +5,15 @@ namespace DoctorHouse.Models
 {
     public class User
     {
-
         public int Id { get; set; }
-
         [Required]
         [Column(TypeName = "varchar(20)")]
         public string Username { get; set; }
-
         [Required]
         [Column(TypeName = "varchar(20)")]
         public string Password { get; set; }
-
         [Required]
         public bool IsAdmin { get; set; }
-
         public UserDetails UserDetails { get; set; }
     }
 }
