1cae0cac7c2c1ff3bef2386b79606608c7b4bd5f
kettu0 <aneta.kruc@gmail.com>
2020-12-29 22:57:06 +0100

    add collection of int - Appointments - to CustomerResource

--- DoctorHouse/Controllers/Resources/CustomerResource.cs
+++ DoctorHouse/Controllers/Resources/CustomerResource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace DoctorHouse.Controllers.Resources
     public class CustomerResource : UserResource
     {
         public string Address { get; set; }
+        public ICollection<int> Appointments { get; set; }
+
+        public CustomerResource()
+        {
+            Appointments = new Collection<int>();
+        }
     }
 }
