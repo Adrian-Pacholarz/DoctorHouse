f88950147728c323d100e0b368c50c28ca68adba
kettu0 <aneta.kruc@gmail.com>
2020-12-29 22:59:51 +0100

    correct UserDetailResource

--- DoctorHouse/Controllers/Resources/UserDetailsResource.cs
+++ DoctorHouse/Controllers/Resources/UserDetailsResource.cs
@@ -12,8 +12,6 @@ namespace DoctorHouse.Controllers.Resources
         public string SecondName { get; set; }
         public string EMail { get; set; }
         public Int64 PhoneNumber { get; set; }
-        public DateTime DateOfRegistration { get; set; }
-        public UserResource User { get; set; }
         public int UserId { get; set; }
     }
 }
