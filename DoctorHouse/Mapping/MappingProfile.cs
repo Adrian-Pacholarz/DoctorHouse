f7373119c7a063899c09a3a5a04e9549ceb97e17
kettu0 <aneta.kruc@gmail.com>
2020-12-31 18:50:04 +0100

    correct mapping after deleting column DetailsId in db

--- DoctorHouse/Mapping/MappingProfile.cs
+++ DoctorHouse/Mapping/MappingProfile.cs
@@ -25,8 +25,8 @@ namespace DoctorHouse.Mapping
                 .ForMember(cr => cr.Username, opt => opt.MapFrom(c => c.Username))
                 .ForMember(cr => cr.Password, opt => opt.MapFrom(c => c.Password))
                 .ForMember(cr => cr.IsAdmin, opt => opt.MapFrom(c => c.IsAdmin))
-                .ForMember(cr => cr.Address, opt => opt.MapFrom(c => c.Address))
-                .ForMember(cr => cr.DetailsId, opt => opt.MapFrom(c => c.DetailsId)); //one-to-one relationship
+                .ForMember(cr => cr.Appointments, opt => opt.MapFrom(c => c.Appointments))
+                .ForMember(cr => cr.Address, opt => opt.MapFrom(c => c.Address));
 
             //Specialist
             CreateMap<Specialist, SpecialistResource>()
@@ -34,10 +34,16 @@ namespace DoctorHouse.Mapping
             .ForMember(sr => sr.Password, opt => opt.MapFrom(s => s.Password))
             .ForMember(sr => sr.IsAdmin, opt => opt.MapFrom(s => s.IsAdmin))
             .ForMember(sr => sr.SpecialistType, opt => opt.MapFrom(s => s.SpecialistType))
+            .ForMember(sr => sr.Appointments, opt => opt.MapFrom(s => s.Appointments))
             .ForMember(sr => sr.Area, opt => opt.MapFrom(s => s.Area))
-            .ForMember(sr => sr.DetailsId, opt => opt.MapFrom(s => s.DetailsId)) //one-to-one relationship
             .ForMember(sr => sr.Companies, opt => opt.MapFrom(s => s.Companies.Select(sc => sc.CompanyId))); //many-to-many relationship
 
+            //User
+            CreateMap<User, UserResource>()
+                .ForMember(ur => ur.Username, opt => opt.MapFrom(u => u.Username))
+                .ForMember(ur => ur.Password, opt => opt.MapFrom(u => u.Password))
+                .ForMember(ur => ur.IsAdmin, opt => opt.MapFrom(u => u.IsAdmin))
+                .ForMember(ur => ur.Details, opt => opt.MapFrom(u => u.Details));
 
 
             //API RESOURCES TO DOMAIN
@@ -45,7 +51,6 @@ namespace DoctorHouse.Mapping
             CreateMap<CustomerResource, Customer>()
                 .ForMember(c => c.Id, opt => opt.Ignore())
                 .ForMember(c => c.Appointments, opt => opt.Ignore())
-                .ForMember(c => c.DetailsId, opt => opt.Ignore())
                 .ForMember(c => c.Username, opt => opt.MapFrom(cr => cr.Username))
                 .ForMember(c => c.Password, opt => opt.MapFrom(cr => cr.Password))
                 .ForMember(c => c.IsAdmin, opt => opt.MapFrom(cr => cr.IsAdmin))
@@ -55,7 +60,6 @@ namespace DoctorHouse.Mapping
             //Specialist
             CreateMap<SpecialistResource, Specialist>()
                 .ForMember(s => s.Id, opt => opt.Ignore())
-                .ForMember(s => s.DetailsId, opt => opt.Ignore())
                 .ForMember(s => s.Appointments, opt => opt.Ignore())
                 .ForMember(s => s.Companies, opt => opt.Ignore())
                 .ForMember(s => s.Username, opt => opt.MapFrom(sr => sr.Username))
@@ -87,7 +91,6 @@ namespace DoctorHouse.Mapping
             //User
             CreateMap<UserResource, User>()
                 .ForMember(u => u.Id, opt => opt.Ignore())
-                .ForMember(u => u.DetailsId, opt => opt.Ignore())
                 .ForMember(u => u.Username, opt => opt.MapFrom(ur => ur.Username))
                 .ForMember(u => u.Password, opt => opt.MapFrom(ur => ur.Password))
                 .ForMember(u => u.IsAdmin, opt => opt.MapFrom(ur => ur.IsAdmin));
