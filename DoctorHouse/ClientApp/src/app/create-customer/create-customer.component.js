1e1640342c46c679a37c0add783c6dee0fef0366
Adrian Pacholarz <flojd_xl@wp.pl>
2021-01-18 03:39:59 +0100

    Add custom validation for area and delete address field in specialist sign up form

--- DoctorHouse/ClientApp/src/app/create-customer/create-customer.component.js
+++ DoctorHouse/ClientApp/src/app/create-customer/create-customer.component.js
@@ -6,115 +6,93 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateSpecialistComponent = void 0;
+exports.CreateCustomerComponent = void 0;
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
-var area_validators_1 = require("../common/validators/area.validators");
 var password_validators_1 = require("../common/validators/password.validators");
 var phone_validators_1 = require("../common/validators/phone.validators");
-var CreateSpecialistComponent = /** @class */ (function () {
-    function CreateSpecialistComponent(specialistService, companiesService) {
-        this.specialistService = specialistService;
-        this.companiesService = companiesService;
+var CreateCustomerComponent = /** @class */ (function () {
+    function CreateCustomerComponent(customerService) {
+        this.customerService = customerService;
         this.signupForm = new forms_1.FormGroup({
             newUsername: new forms_1.FormControl('', [forms_1.Validators.required, forms_1.Validators.minLength(6)]),
             newName: new forms_1.FormControl('', forms_1.Validators.required),
             newSurname: new forms_1.FormControl('', forms_1.Validators.required),
             newPhone: new forms_1.FormControl('', [forms_1.Validators.required, phone_validators_1.PhoneValidators.phoneIsNaN, phone_validators_1.PhoneValidators.phoneLength]),
-            newArea: new forms_1.FormControl('', [forms_1.Validators.required, area_validators_1.AreaValidators.areaIsNanAndMoreThan30]),
             newEmail: new forms_1.FormControl('', [forms_1.Validators.required, forms_1.Validators.email]),
-            newSpecialization: new forms_1.FormControl('', forms_1.Validators.required),
-            newCompanies: new forms_1.FormControl('', forms_1.Validators.required),
             passwords: new forms_1.FormGroup({
                 newPassword: new forms_1.FormControl('', [forms_1.Validators.required, forms_1.Validators.minLength(6)]),
                 repeatPassword: new forms_1.FormControl('', forms_1.Validators.required)
             }, password_validators_1.PasswordValidators.passwordsMustMatch)
         });
     }
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newUsername", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newUsername", {
         get: function () {
             return this.signupForm.get('newUsername');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newName", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newName", {
         get: function () {
             return this.signupForm.get('newName');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newSurname", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newSurname", {
         get: function () {
             return this.signupForm.get('newSurname');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newPhone", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newAddress", {
+        get: function () {
+            return this.signupForm.get('newAddress');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreateCustomerComponent.prototype, "newPhone", {
         get: function () {
             return this.signupForm.get('newPhone');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newArea", {
-        get: function () {
-            return this.signupForm.get('newArea');
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newEmail", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newEmail", {
         get: function () {
             return this.signupForm.get('newEmail');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newSpecialization", {
-        get: function () {
-            return this.signupForm.get('newSpecialization');
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newCompanies", {
-        get: function () {
-            return this.signupForm.get('newCompanies');
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "newPassword", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "newPassword", {
         get: function () {
             return this.signupForm.get('passwords.newPassword');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "repeatPassword", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "repeatPassword", {
         get: function () {
             return this.signupForm.get('passwords.repeatPassword');
         },
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(CreateSpecialistComponent.prototype, "passwords", {
+    Object.defineProperty(CreateCustomerComponent.prototype, "passwords", {
         get: function () {
             return this.signupForm.get('passwords');
         },
         enumerable: false,
         configurable: true
     });
-    CreateSpecialistComponent.prototype.submit = function () {
-        var newSpecialist = {
+    CreateCustomerComponent.prototype.submit = function () {
+        var newCustomer = {
             username: this.newUsername.value,
             password: this.newPassword.value,
-            area: +this.newArea.value,
-            specialistType: this.newSpecialization.value,
-            companies: this.newCompanies.value,
             details: {
                 firstName: this.newName.value,
                 lastName: this.newSurname.value,
@@ -122,27 +100,18 @@ var CreateSpecialistComponent = /** @class */ (function () {
                 phoneNumber: +this.newPhone.value,
             }
         };
-        var companiesIds = [];
-        newSpecialist.companies.forEach(function (value) {
-            companiesIds.push(+value);
-        });
-        newSpecialist.companies = companiesIds;
-        this.specialistService.createSpecialist(newSpecialist).subscribe(function (x) { return console.log(x); });
+        this.customerService.createCustomer(newCustomer).subscribe(function (x) { return console.log(x); });
     };
-    CreateSpecialistComponent.prototype.ngOnInit = function () {
-        var _this = this;
-        this.companiesService.getCompanies().subscribe(function (companies) {
-            _this.allCompanies = companies;
-        });
+    CreateCustomerComponent.prototype.ngOnInit = function () {
     };
-    CreateSpecialistComponent = __decorate([
+    CreateCustomerComponent = __decorate([
         core_1.Component({
-            selector: 'app-create-specialist',
-            templateUrl: './create-specialist.component.html',
-            styleUrls: ['./create-specialist.component.css']
+            selector: 'app-create-customer',
+            templateUrl: './create-customer.component.html',
+            styleUrls: ['./create-customer.component.css']
         })
-    ], CreateSpecialistComponent);
-    return CreateSpecialistComponent;
+    ], CreateCustomerComponent);
+    return CreateCustomerComponent;
 }());
-exports.CreateSpecialistComponent = CreateSpecialistComponent;
-//# sourceMappingURL=create-specialist.component.js.map
+exports.CreateCustomerComponent = CreateCustomerComponent;
+//# sourceMappingURL=create-customer.component.js.map
--- DoctorHouse/ClientApp/src/app/create-specialist/create-specialist.component.ts
+++ DoctorHouse/ClientApp/src/app/create-specialist/create-specialist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { AreaValidators } from '../common/validators/area.validators';
 import { PasswordValidators } from '../common/validators/password.validators';
 import { PhoneValidators } from '../common/validators/phone.validators';
 import { CompaniesService } from '../services/companies.service';
@@ -18,9 +19,8 @@ export class CreateSpecialistComponent implements OnInit {
     newUsername: new FormControl('', [Validators.required, Validators.minLength(6)]),
     newName: new FormControl('', Validators.required),
     newSurname: new FormControl('', Validators.required),
-    newAddress: new FormControl('', Validators.required),
     newPhone: new FormControl('', [Validators.required, PhoneValidators.phoneIsNaN, PhoneValidators.phoneLength]),
-    newArea: new FormControl('', [Validators.required, PhoneValidators.phoneIsNaN, Validators.maxLength(2)]), 
+    newArea: new FormControl('', [Validators.required, AreaValidators.areaIsNanAndMoreThan30]), 
     newEmail: new FormControl('', [Validators.required, Validators.email]),
     newSpecialization: new FormControl('', Validators.required),
     newCompanies: new FormControl('', Validators.required),
@@ -40,10 +40,6 @@ export class CreateSpecialistComponent implements OnInit {
 
   get newSurname() {
     return this.signupForm.get('newSurname');
-  }
-
-  get newAddress() {
-    return this.signupForm.get('newAddress');
   }
 
   get newPhone() {
@@ -82,7 +78,6 @@ export class CreateSpecialistComponent implements OnInit {
   submit() {
 
     let newSpecialist = {
-      address: this.newAddress.value,
       username: this.newUsername.value,
       password: this.newPassword.value,
       area: +this.newArea.value,
