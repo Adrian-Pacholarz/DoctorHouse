579853ac37038f93f379a54187757d690fccc69f
Adrian Pacholarz <flojd_xl@wp.pl>
2021-01-19 01:54:09 +0100

    Implement authentication method to generate token on login in api

---
+++ DoctorHouse/Core/AuthenticateModels/AuthenticateResponse.cs
@@ -0,0 +1,27 @@
+﻿using DoctorHouse.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorHouse.Core.AuthenticateModels
+{
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+
+        public AuthenticateResponse(User user, string token)
+        {
+            Id = user.Id;
+            FirstName = user.Details.FirstName;
+            LastName = user.Details.LastName;
+            Username = user.Username;
+            Token = token;
+        }
+    }
+}
